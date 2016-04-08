559f45bcf159bbd8c30727ab2766c61fa274ca17
liudf0716 <liudf0716@gmai.com>
2016-04-08 15:33:48 +0900

    liudf 20160408	debug

--- src/conf.h
+++ src/conf.h
@@ -244,11 +244,11 @@ typedef struct {
 	t_trusted_mac	*roam_maclist; /** roam mac list*/
 	t_untrusted_mac	*mac_blacklist; /** blacklist mac*/
 	char 	*htmlredirfile;
+	int		wired_passed; 
 	short	js_filter; /** boolean, whether to enable javascript filter url request*/
 	short	pool_mode;
 	short	thread_number;
 	short	queue_size;
-	int		wired_passed; 
 } s_config;
 
 /** @brief Get the current gateway configuration */
