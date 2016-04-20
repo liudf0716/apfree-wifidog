8c1ce1c442b636585fa561218799d130e1f93af1
liudf0716 <liudf0716@gmai.com>
2016-04-20 16:18:39 +0900

    liudf 20160420	Change whitelist mac to wireless state

--- src/firewall.c
+++ src/firewall.c
@@ -415,7 +415,7 @@ update_trusted_mac_list_status(void)
 		debug(LOG_INFO, "update_trusted_mac_list_status: %s %s %d", p1->ip, p1->mac, p1->is_online);
 		if (config->auth_servers != NULL && p1->is_online) {
             auth_server_request(&authresponse, REQUEST_TYPE_COUNTERS, p1->ip, p1->mac, "null", 0,
-                                0, 0, 0, 0, 0, "null", 0);
+                                0, 0, 0, 0, 0, "null", 1);
         }
 			
 	}
