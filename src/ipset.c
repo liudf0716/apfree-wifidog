bbd07b69b38253df25b71011d4d5d902a2e36772
liudf0716 <liudf0716@gmai.com>
2016-05-20 20:58:52 +0900

    liudf 20160520	debug...

--- src/ipset.c
+++ src/ipset.c
@@ -107,7 +107,7 @@ static int new_add_to_ipset(const char *setname, const struct in_addr *ipaddr, i
 	uint8_t proto;
 	int addrsz = INADDRSZ;
 
-	debug(LOG_DEBUG, "new_add_to_ipset ====== ");
+	debug(LOG_INFO, "new_add_to_ipset ====== ");
 
 	if (strlen(setname) >= IPSET_MAXNAMELEN) 
 	{
