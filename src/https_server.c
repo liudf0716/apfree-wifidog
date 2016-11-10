5063b7a38c26cd59c25df4121b87c36182564249
Dengfeng Liu <liudf0716@gmail.com>
2016-11-10 18:25:10 +0800

    Update https_server.c

--- src/https_server.c
+++ src/https_server.c
@@ -1,4 +1,40 @@
+
+#include <netinet/in.h>  /* INET6_ADDRSTRLEN */
+#include <stdlib.h>
+#include <string.h>
+#include <uv.h>
+
 #include "https_server.h"
 
+uv_loop_t *loop;
+
+void on_new_connection(uv_stream_t *server, int status) {
+    if (status == -1) {
+        return;
+    }
+
+    uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+    uv_tcp_init(loop, client);
+    if (uv_accept(server, (uv_stream_t*) client) == 0) {
+        uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+    } else {
+        uv_close((uv_handle_t*) client, NULL);
+    }
+}
+
+
 void thread_https_server(void *args) {
+    loop = uv_default_loop();
+
+    uv_tcp_t server;
+    uv_tcp_init(loop, &server);
+
+    struct sockaddr_in bind_addr = uv_ip4_addr("0.0.0.0", 8443);
+    uv_tcp_bind(&server, bind_addr);
+    int r = uv_listen((uv_stream_t*) &server, 128, on_new_connection);
+    if (r) {
+        fprintf(stderr, "Listen error!\n");
+        return 1;
+    }
+    return uv_run(loop, UV_RUN_DEFAULT);
 }
