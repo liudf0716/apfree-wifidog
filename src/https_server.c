0bcfcb34cf58610c4e38c52134ea0df1376a098e
liudf0716 <liudf0716@gmai.com>
2016-11-10 18:05:42 +0900

    liudf 20161110	Add https_server.c file to project

---
+++ src/https_server.c
@@ -0,0 +1,4 @@
+#include "https_server.h"
+
+void thread_https_server(void *args) {
+}
---
+++ src/https_server.h
@@ -0,0 +1,4 @@
+#ifndef	_HTTPS_SERVER_H_
+#define	_HTTPS_SERVER_H_
+void thread_https_server(void *args);
+#endif
