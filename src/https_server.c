e4ca19cce4d0f8e65f694b933c36ce6240371637
Dengfeng Liu <liudf0716@gmail.com>
2016-11-16 14:32:49 +0800

    Update https_server.c

--- src/https_server.c
+++ src/https_server.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <uv.h>
+#include <syslog.h>
 #include "uvhttp.h"
 #include "https_server.h"
 #include "debug.h"
@@ -14,14 +15,12 @@ void thread_https_server(void *args) {
         if ( server_ssl) {
             const s_config *config = config_get_config();
             uvhttp_server_set_option( server_ssl, UVHTTP_SRV_OPT_SSL, 1);
-            if ( uvhttp_server_ip4_listen( server_ssl, config->gw_address, config->https_server->ssl_port) == UVHTTP_OK) {
-                debug(LOG_INFO, "https on %d success", config->https_server->ssl_port);
+            if ( uvhttp_server_ip4_listen( server_ssl, config->gw_address, config->gw_https_port) == UVHTTP_OK) {
+                debug(LOG_INFO, "https on %d success", config->gw_https_port);
             }
         }
         uvhttp_run( loop);
         uvhttp_server_delete( server_ssl);
         uvhttp_loop_delete( loop);
     }
-    
-    return 0;
 }
