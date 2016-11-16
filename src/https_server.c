7b4d945b50aafcb21e24d10688d2d5a48a33138f
Dengfeng Liu <liudf0716@gmail.com>
2016-11-16 15:48:41 +0800

    Update https_server.c

--- src/https_server.c
+++ src/https_server.c
@@ -15,8 +15,8 @@ void thread_https_server(void *args) {
         if ( server_ssl) {
             const s_config *config = config_get_config();
             uvhttp_server_set_option( server_ssl, UVHTTP_SRV_OPT_SSL, 1);
-            if ( uvhttp_server_ip4_listen( server_ssl, config->gw_address, config->gw_https_port) == UVHTTP_OK) {
-                debug(LOG_INFO, "https on %d success", config->gw_https_port);
+            if ( uvhttp_server_ip4_listen( server_ssl, config->gw_address, config->https_server->gw_https_port) == UVHTTP_OK) {
+                debug(LOG_INFO, "https on %d success", config->https_server->gw_https_port);
             }
         }
         uvhttp_run( loop);
