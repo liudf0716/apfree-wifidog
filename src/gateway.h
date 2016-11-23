7313175767ae43afea3f783f12c48df7b50f0f26
Dengfeng Liu <liudf0716@gmail.com>
2016-11-23 17:29:44 +0800

    Update gateway.h

--- src/gateway.h
+++ src/gateway.h
@@ -29,7 +29,12 @@
 #define _GATEWAY_H_
 
 #include <stdio.h>
-#include <event2/evbuffer-internal.h>
+
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/keyvalq_struct.h>
 
 #include "httpd.h"
 
