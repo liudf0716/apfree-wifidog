38e8273996753164526e564adcef5f3a8d658fce
Dengfeng Liu <liudf0716@gmail.com>
2016-11-25 11:52:49 +0800

    Update https_server.h

--- src/https_server.h
+++ src/https_server.h
@@ -2,5 +2,8 @@
 #define	_HTTPS_SERVER_H_
 void thread_https_server(void *args);
 
-struct evbuffer *get_full_redir_url(const char *mac, const char *ip, const char *orig_url);
+char*get_full_redir_url(const char *mac, const char *ip, const char *orig_url);
+void evhttpd_gw_reply(struct evhttp_request *req, enum reply_type type);
+char *evhttp_get_request_url(struct evhttp_request *req);
+
 #endif
