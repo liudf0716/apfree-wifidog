9c7431de92ec1978b56fe7f14198b98289114a93
Dengfeng Liu <liudf0716@gmail.com>
2016-11-25 11:13:14 +0800

    Update gateway.c

--- src/gateway.c
+++ src/gateway.c
@@ -73,11 +73,6 @@
 #include "ipset.h"
 #include "https_server.h"
 
-struct redir_file_buffer {
-    struct evbuffer *evb_front;
-    struct evbuffer *evb_rear;
-};
-
 struct evbuffer	*evb_internet_offline_page 		= NULL;
 struct evbuffer *evb_authserver_offline_page	= NULL;
 struct redir_file_buffer *wifidog_redir_html 	= NULL;
