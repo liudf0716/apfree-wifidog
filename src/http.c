9b11ab85458f608f4e3c17d9bfe865b2fdcd7e7c
Dengfeng Liu <liudf0716@gmail.com>
2016-11-25 17:09:01 +0800

    Update http.c

--- src/http.c
+++ src/http.c
@@ -25,6 +25,7 @@
   @author Copyright (C) 2004 Philippe April <papril777@yahoo.com>
   @author Copyright (C) 2007 Benoit Grégoire
   @author Copyright (C) 2007 David Bird <david@coova.com>
+  @author Copyright (C) 2016 Dengfeng Liu <liudengfeng@kunteng.org>
 
  */
 /* Note that libcs other than GLIBC also use this macro to enable vasprintf */
