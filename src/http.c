f15eec5c9db9001fa35e17cb87becf076aa7eca7
Dengfeng Liu <liudf0716@gmail.com>
2016-11-28 20:17:14 +0800

    Update http.c

--- src/http.c
+++ src/http.c
@@ -450,7 +450,7 @@ http_send_js_redirect(request *r, const char *redir_url)
 	evbuffer_add(evb, wifidog_redir_html->front, wifidog_redir_html->front_len);
 	evbuffer_add_printf(evb_redir_url, WIFIDOG_REDIR_HTML_CONTENT, redir_url);
 	evbuffer_add_buffer(evb, evb_redir_url);
-	evbuffer_add(evb, wifidog_redir_html->evb_rear, wifidog_redir_html->evb_len);
+	evbuffer_add(evb, wifidog_redir_html->rear, wifidog_redir_html->rear_len);
 	
 	char *redirect_html = evb_2_string(evb);
     
