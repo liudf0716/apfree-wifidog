31f29a9d2c2e72c561838c75fb3a194b842f3043
Dengfeng Liu <liudf0716@gmail.com>
2016-11-28 18:09:58 +0800

    Update gateway.c

--- src/gateway.c
+++ src/gateway.c
@@ -24,6 +24,7 @@
   @brief Main loop
   @author Copyright (C) 2004 Philippe April <papril777@yahoo.com>
   @author Copyright (C) 2004 Alexandre Carmel-Veilleux <acv@miniguru.ca>
+  @author Copyright (C) 2016 Dengfeng Liu <liudengfeng@kunteng.org>
  */
 
 #include <stdio.h>
