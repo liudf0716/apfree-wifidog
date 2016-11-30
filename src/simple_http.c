d25677d4348414d0755fba09a12e2ab21d36624f
Dengfeng Liu <liudf0716@gmail.com>
2016-11-30 17:00:19 +0800

    Update simple_http.c

--- src/simple_http.c
+++ src/simple_http.c
@@ -31,6 +31,8 @@
 #include <string.h>
 #include <syslog.h>
 
+#include <zlib/zlib.h>
+
 #include "common.h"
 #include "debug.h"
 #include "pstring.h"
@@ -49,6 +51,94 @@
 #ifdef USE_CYASSL
 static CYASSL_CTX *get_cyassl_ctx(const char *hostname);
 #endif
+
+int 
+inflate_read(char *source, int len, char **dest, int gzip)
+{  
+	int ret;  
+	unsigned have;  
+	z_stream strm;  
+	unsigned char out[CHUNK];  
+	int totalsize = 0;  
+
+	/* allocate inflate state */  
+	strm.zalloc = Z_NULL;  
+	strm.zfree = Z_NULL;  
+	strm.opaque = Z_NULL;  
+	strm.avail_in = 0;  
+	strm.next_in = Z_NULL;  
+
+	if(gzip)  
+		ret = inflateInit2(&strm, 47);  
+	else  
+		ret = inflateInit(&strm);  
+
+	if (ret != Z_OK)  
+		return ret;  
+
+	strm.avail_in = len;  
+	strm.next_in = source;  
+
+	/* run inflate() on input until output buffer not full */  
+	do {  
+		strm.avail_out = CHUNK;  
+		strm.next_out = out;  
+		ret = inflate(&strm, Z_NO_FLUSH);  
+		assert(ret != Z_STREAM_ERROR); /* state not clobbered */  
+		switch (ret) {  
+		case Z_NEED_DICT:  
+			ret = Z_DATA_ERROR; /* and fall through */  
+		case Z_DATA_ERROR:  
+		case Z_MEM_ERROR:  
+			inflateEnd(&strm);  
+			return ret;  
+		}  
+		have = CHUNK - strm.avail_out;  
+		totalsize += have;  
+		*dest = realloc(*dest, totalsize);  
+		memcpy(*dest + totalsize - have, out, have);  
+	} while (strm.avail_out == 0);  
+
+	/* clean up and return */  
+	(void)inflateEnd(&strm);  
+	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;  
+}
+
+/*
+ * this function assume u know this http request get deflate response
+ */
+char *
+http_get_uncompressed(const int sockfd, const char *req)
+{
+	char *uncompressed = NULL;
+	char *http_response = http_get(sockfd, req);
+	if (!http_response) {
+		goto err;
+	}
+	
+	char *presponse = strstr(http_response, "Content-Encoding: deflate");
+	if (! presponse)
+		goto err;
+	
+	char *pcontent = strstr(presponse+strlen("Content-Encoding: deflate"), "\r\n\r\n");
+	if (!pcontent) 
+		goto err;
+	
+	pcontent += strlen("\r\n\r\n");
+		
+	int ret = inflate_read (pcontent, strlen(pcontent), &uncompressed, 1);
+	if (ret != Z_OK) {
+		goto err;
+	}
+	
+	debug(LOG_INFO, "uncompressed is OK, its length is %d", strlen(uncompressed));
+	free(http_response);
+	return uncompressed;
+err:
+	if (!uncompressed) free(uncompressed);
+	if (!http_response) free(http_response);
+	return NULL;
+}
 
 char *
 http_get(const int sockfd, const char *req)
