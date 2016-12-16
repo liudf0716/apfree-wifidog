593e225dc7c65714cd33174ceb202ba5d5cce9ca
Dengfeng Liu <liudf0716@gmail.com>
2016-12-16 15:03:44 +0800

    Update util.c

--- src/util.c
+++ src/util.c
@@ -461,9 +461,10 @@ is_valid_mac(const char *mac)
 	return (i == 12 && (s == 5 || s == 0));
 }
 
-char *evb_2_string(struct evbuffer *evb) 
+char *evb_2_string(struct evbuffer *evb, int *olen) 
 {
-	size_t len = evbuffer_get_length(evb)+1;
+	*olen = evbuffer_get_length(evb);
+	size_t len = *olen + 1;
 	char *str = (char *)malloc(len);
 	memset(str, 0, len);
 	evbuffer_copyout(evb, str, len);
