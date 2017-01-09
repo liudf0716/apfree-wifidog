68295dfbe196f0655ffcf8f44966ae2cad78a7c8
Dengfeng Liu <liudf0716@qq.com>
2017-01-09 14:27:54 +0800

    Update util.h

--- src/util.h
+++ src/util.h
@@ -28,35 +28,8 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-
-
 #include <sys/socket.h>
 
-
-
-/** How many times should we try detecting the interface with the default route
- * (in seconds).  If set to 0, it will keep retrying forever */
-#define NUM_EXT_INTERFACE_DETECT_RETRY 0
-/** How often should we try to detect the interface with the default route
- *  if it isn't up yet (interval in seconds) */
-#define EXT_INTERFACE_DETECT_RETRY_INTERVAL 1
-
-
-
-/** @brief Execute a shell command */
-int execute(const char *, int);
-
-/** @brief Thread safe gethostbyname */
-struct in_addr *wd_gethostbyname(const char *);
-
-/** @brief Get IP address of an interface */
-char *get_iface_ip(const char *);
-
-/** @brief Get MAC address of an interface */
-char *get_iface_mac(const char *);
-
-/** @brief Get interface name of default gateway */
-char *get_ext_iface(void);
 
 /** @brief Initialize the ICMP socket */
 int init_icmp_socket(void);
@@ -77,4 +50,5 @@ int is_valid_mac(const char *);
 int is_socket_valid(int );
 
 int wd_connect(int, const struct sockaddr *, socklen_t, int);
+
 #endif                          /* _UTIL_H_ */
