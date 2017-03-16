84858c59071a7cd41b117e187821b449bfc583bc
Dengfeng Liu <liudf0716@qq.com>
2017-03-16 15:33:58 +0800

    Update wd_util.c

--- src/wd_util.c
+++ src/wd_util.c
@@ -875,10 +875,10 @@ void evdns_parse_trusted_domain_2_ip(t_domain_trusted *p)
     // thanks the following article
     // http://www.wuqiong.info/archives/13/
     evdns_base_set_option(dnsbase, "randomize-case:", "0");//TurnOff DNS-0x20 encoding
-    evdns_base_nameserver_ip_add(dnsbase, "180.76.76.76");//BaiduDNS
-    evdns_base_nameserver_ip_add(dnsbase, "223.5.5.5");//AliDNS
-    evdns_base_nameserver_ip_add(dnsbase, "223.6.6.6");//AliDNS
-    evdns_base_nameserver_ip_add(dnsbase, "114.114.114.114");//114DNS
+    //evdns_base_nameserver_ip_add(dnsbase, "180.76.76.76");//BaiduDNS
+    //evdns_base_nameserver_ip_add(dnsbase, "223.5.5.5");//AliDNS
+    //evdns_base_nameserver_ip_add(dnsbase, "223.6.6.6");//AliDNS
+    //evdns_base_nameserver_ip_add(dnsbase, "114.114.114.114");//114DNS
 	
 	struct evutil_addrinfo hints;
 	n_pending_requests = 0;
