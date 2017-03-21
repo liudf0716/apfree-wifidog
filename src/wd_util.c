cdaecf0d76e41e46fec74309bfa4a8a57a7206f4
Dengfeng Liu <liudf0716@qq.com>
2017-03-21 20:30:49 +0800

    debug

--- src/wd_util.c
+++ src/wd_util.c
@@ -810,7 +810,7 @@ is_br_port_no_wired(const char *brname, int port_no)
 // if -1; not find mac; else find mac's port_no
 static int 
 br_read_fdb(const char *bridge, struct fdb_entry *fdbs,
-        unsigned long offset, int num, const uint8_t *mac, int *port_no)
+        unsigned long offset, int num, const uint8_t *mac_addr, int *port_no)
 {
     FILE *f;
     int i, n = 0;
