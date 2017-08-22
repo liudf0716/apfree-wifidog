ca2cfc3aa86c7844578fd479416c53fa26cc0f77
Dengfeng Liu <liudf0716@qq.com>
2017-08-22 14:47:05 +0800

    Update wd_util.h

--- src/wd_util.h
+++ src/wd_util.h
@@ -100,6 +100,7 @@ char *mqtt_get_trusted_iplist_text(void);
 char *get_trusted_domains_text(void);
 char *get_untrusted_maclist_text(void);
 char *get_trusted_maclist_text(void);
+char *get_trusted_local_maclist_text(void);
 char *get_roam_maclist_text(void);
 char *get_serialize_trusted_pan_domains(void);
 char *mqtt_get_trusted_pan_domains_text(void);
