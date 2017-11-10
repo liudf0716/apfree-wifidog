0738b19ce410b526cfc711e956d2c7d74d7d27cb
Dengfeng Liu <liudf0716@qq.com>
2017-11-10 17:59:25 +0800

    add check_device_online
    check trusted macs whether online or not

--- src/https_server.c
+++ src/https_server.c
@@ -82,6 +82,7 @@
 #endif
 
 #include <syslog.h>
+#include <events/ping.h>
 
 #include "https_server.h"
 #include "debug.h"
@@ -94,6 +95,7 @@
 
 static struct event_base *base		= NULL;
 static struct evdns_base *dnsbase 	= NULL;
+static struct evping_base *pingbase	= NULL;
 
 static void check_internet_available_cb(int errcode, struct evutil_addrinfo *addr, void *ptr);
 
@@ -333,6 +335,50 @@ static void check_auth_server_available() {
               &hints, check_auth_server_available_cb, auth_server);
 }
 
+static void check_device_online_cb(int result, int bytes, char * fqname, char * dotname,
+              int seq, int ttl, struct timeval * elapsed, void * arg) {
+	t_trusted_mac *tmac = get_trusted_mac_by_ip(dotname);
+	if (!tmac)
+		return;	
+	
+	switch(result) {
+	case PING_ERR_NONE：
+		tmac->is_online = 1;
+		break;
+	case PING_ERR_TIMEOUT:
+	default:
+		tmac->is_online = 0;
+		break;	
+	}
+	
+	evping_base_host_delete_by_ip(pingbase, tmac->ip);
+}
+
+//check whether white mac device online or not
+static void check_device_online() {
+	t_trusted_mac *p1 = NULL, *tmac_list = NULL;
+	s_config *config = config_get_config();
+	tmac_list = config.trustedmaclist;
+	if (!tmac_list)
+		return;
+	
+	int flag = 0;
+	LOCK_CONFIG();
+	for(p1 = tmac_list; p1 != NULL; p1 = p1->next) {
+		if(p->ip == NULL) 
+			p->ip = arp_get_ip(tmac->mac);
+		
+		if (p->ip) {
+			evping_base_host_add(pingbase, p->ip);
+			flag = 1;
+		}
+	}
+ 	UNLOCK_CONFIG();
+	
+	if (flag)
+		evping_ping (pingbase, check_device_online_cb, tmac_list);
+}
+
 static void schedule_work_cb(evutil_socket_t fd, short event, void *arg) {
 	struct event *timeout = (struct event *)arg;
 	struct timeval tv;
@@ -342,6 +388,8 @@ static void schedule_work_cb(evutil_socket_t fd, short event, void *arg) {
 	check_internet_available(popular_server);
 
 	check_auth_server_available();
+	
+	check_device_online();
 	
 	// if config->update_domain_interval not 0
 	if (update_domain_interval == config_get_config()->update_domain_interval) {
@@ -360,23 +408,33 @@ static void schedule_work_cb(evutil_socket_t fd, short event, void *arg) {
 	event_add(timeout, &tv);
 }
 
-static int https_redirect (char *gw_ip,  t_https_server *https_server) { 	
+static int init_https_redirect (char *gw_ip,  t_https_server *https_server) { 	
   	struct evhttp *http;
   	struct evhttp_bound_socket *handle;
 	struct event timeout;
 	struct timeval tv;
+	int nret = 0;
 	
   	base = event_base_new ();
-  	if (! base) { 
+  	if (!base) { 
 		debug (LOG_ERR, "Couldn't create an event_base: exiting\n");
-      	return 1;
+      	nret = 1;
+		goto err;
     }
+	
+	pingbase = evping_base_new(base);
+	if (!pingbase) {
+		debug(LOG_ERR, "Couldn't create evping_base: exiting\n");
+		nret = 1;
+		goto err;
+	}
 	
   	/* Create a new evhttp object to handle requests. */
   	http = evhttp_new (base);
   	if (! http) { 
 		debug (LOG_ERR, "couldn't create evhttp. Exiting.\n");
-      	return 1;
+		nret = 1;
+		goto err;
     }
  
  	SSL_CTX *ctx = SSL_CTX_new (SSLv23_server_method ());
@@ -407,15 +465,19 @@ static int https_redirect (char *gw_ip,  t_https_server *https_server) {
 	if (! handle) { 
 		debug (LOG_ERR, "couldn't bind to port %d. Exiting.\n",
                (int) https_server->gw_https_port);
-		return 1;
+		nret = 1;
+		goto err;
     }
     
 	// check whether internet available or not
 	dnsbase = evdns_base_new(base, 0);
-	if ( 0 != evdns_base_resolv_conf_parse(dnsbase, DNS_OPTION_NAMESERVERS, "/tmp/resolv.conf.auto") ) {
+	if (dnsbase && 0 != evdns_base_resolv_conf_parse(dnsbase, DNS_OPTION_NAMESERVERS, "/tmp/resolv.conf.auto") ) {
 		debug (LOG_ERR, "evdns_base_resolv_conf_parse failed \n");
 		evdns_base_free(dnsbase, 0);
 		dnsbase = evdns_base_new(base, 1);
+	} else if (!dnsbase) {
+		nret = 1;
+		goto err;
 	}
 	evdns_base_set_option(dnsbase, "timeout", config_get_config()->dns_timeout);
 	evdns_base_set_option(dnsbase, "randomize-case:", "0");//TurnOff DNS-0x20 encoding
@@ -433,15 +495,19 @@ static int https_redirect (char *gw_ip,  t_https_server *https_server) {
     event_base_dispatch (base);
 
 	event_del(&timeout);
-	event_base_free(base);
-	evdns_base_free(dnsbase, 0);
-	close(handle);
+
+err:
+	if (handle) close(handle);
+	if (dnsbase) evdns_base_free(dnsbase, 0);
+	if (pingbase) evping_base_free(pingbase, 0);
+	if (base) event_base_free(base);
+	
 	
   	/* not reached; runs forever */
-  	return 0;
+  	return nret;
 }
 
 void thread_https_server(void *args) {
 	s_config *config = config_get_config();
-   	https_redirect (config->gw_address, config->https_server);
+   	init_https_redirect (config->gw_address, config->https_server);
 }
