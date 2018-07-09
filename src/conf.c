575cafa4c94bc895f62af1d425ffb7166d53a072
Dengfeng Liu <liudengfeng@kunteng.org>
2018-07-09 17:00:20 +0800

    Update conf.c

--- src/conf.c
+++ src/conf.c
@@ -70,7 +70,7 @@
 
 
 //>>> liudf added 20160114
-const char	*g_inner_trusted_domains = "www.kunteng.org,cloud.kunteng.org,mqtt.kunteng.org,log1.kunteng.org";
+const char	*g_inner_trusted_domains = "www.kunteng.org.cn,wfc.wificoin.club,talkblock.org,mqtt.kunteng.org.cn,log1.kunteng.org.cn";
 
 /** @internal
  * Holds the current configuration of the gateway */
