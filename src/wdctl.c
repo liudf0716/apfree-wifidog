7ebfadb63dd6f8135cd6fb99f22b6a84fc33b876
liudengfeng <liudengfeng@kunteng.org>
2018-10-18 16:30:37 +0800

    add wdctlx help command
    Signed-off-by: liudengfeng <liudengfeng@kunteng.org>

--- src/wdctl.c
+++ src/wdctl.c
@@ -48,18 +48,17 @@ static int connect_to_server(const char *);
 static struct wdctl_client_command {
     const char *command; // command name
     const char *cmd_args; // comand args demo
-    const char *cmd_help; // help
+    const char *cmd_description; // help
 } wdctl_clt_cmd [] = {
-    {"status", "", "get apfree wifidog status"},
-    {"stop", "", "stop apfree wifidog"},
-    {"clear_trusted_pdomains", "", "clear trusted pan-domain"},
-    {"show_trusted_pdomains", "", "show trusted pan-domain"},
-    {"clear_trusted_iplist", "", "clear trusted iplist"},
-    {"reparse_trusted_domains", "", "reparse trusted domain's ip and add new parsed ip"},
-    {"clear_trusted_domains", "", "clear trusted domain and it's ip"},
-    {"show_trusted_domains", "", "show trusted domains and its ip"},
-    {"show_trusted_mac", "", "show trusted mac list"},
-    {"clear_trusted_mac", "", "clear trusted mac list"},
+    {"status", NULL, "get apfree wifidog status"},
+    {"clear_trusted_pdomains", NULL, "clear trusted pan-domain"},
+    {"show_trusted_pdomains", NULL, "show trusted pan-domain"},
+    {"clear_trusted_iplist", NULL, "clear trusted iplist"},
+    {"reparse_trusted_domains", NULL, "reparse trusted domain's ip and add new parsed ip"},
+    {"clear_trusted_domains", NULL, "clear trusted domain and it's ip"},
+    {"show_trusted_domains", NULL, "show trusted domains and its ip"},
+    {"show_trusted_mac", NULL, "show trusted mac list"},
+    {"clear_trusted_mac", NULL, "clear trusted mac list"},
     {"add_trusted_pdomains", "pan-domain1,pan-domain2...", "add one or more trusted pan-domain like kunteng.org.cn,qq.com..."},
     {"del_trusted_pdomains", "pan-domain1,pan-domain2...", "del one or more trusted pan-domain list like kunteng.org.cn,qq.com..."},
     {"add_trusted_domains", "domain1,domain2...", "add trusted domain list like www.kunteng.org.cn,www.qq.com..."},
@@ -69,8 +68,10 @@ static struct wdctl_client_command {
     {"add_trusted_mac", "mac1,mac2...", "add one or more trusted mac list like mac1,mac2..."},
     {"del_trusted_mac", "mac1,mac2...", "del one or more trusted mac list like mac1,mac2..."},
     {"add_online_client", "{\"ip\":\"ipaddress\", \"mac\":\"devMac\", \"name\":\"devName\"}", "add client to connected list "},
-    {"user_cfg_save", "", "save all rule to config file"},
+    {"user_cfg_save", NULL, "save all rule to config file"},
     {"reset", "ip|mac", "logout connected client by its ip or mac"},
+    {"stop", NULL, "stop apfree wifidog"},
+    {"help", NULL, "list all method"},
 };
 
 /** 
@@ -88,11 +89,37 @@ usage(void)
     fprintf(stdout, "  -s <path>         Path to the socket\n");
     fprintf(stdout, "  -h                Print usage\n");
     fprintf(stdout, "\n");
-    fprintf(stdout, "commands:\n");
+    fprintf(stdout, "commands arg\t description:\n");
     for (int i = 0; i < ARRAYLEN(wdctl_clt_cmd); i++) {
-        fprintf(stdout, " %s %s\t %s \n",
-            wdctl_clt_cmd[i].command, wdctl_clt_cmd[i].cmd_args, wdctl_clt_cmd[i].cmd_help);
+        fprintf(stdout, " %s %s\t %s \n", wdctl_clt_cmd[i].command, 
+            wdctl_clt_cmd[i].cmd_args?wdctl_clt_cmd[i].cmd_args:"", 
+            wdctl_clt_cmd[i].cmd_description);
     }
+}
+
+static void
+list_all_method()
+{
+#define COMMAND_EQUAL(CMD) !strcmp(cmd,CMD)
+    char *cmd = NULL;
+    for (int i = 0; i < ARRAYLEN(wdctl_clt_cmd); i++) {
+        cmd = wdctl_clt_cmd[i].command;
+        if (COMMAND_EQUAL("list"))
+            continue;
+        else if(COMMAND_EQUAL("add_online_client"))
+            fprintf(stdout, "%s {\"ip\":\"192.168.1.211\", \"mac\":\"aa:bb:cc:dd:ee:ff\", \"name\":\"apfree\"}\n", cmd);
+        else if (COMMAND_EQUAL("add_trusted_domains"))
+            fpintf(stdout, "%s www.kunteng.org.cn,captive.apple.com,www.baidu.com,www.qq.com,www.alibaba.com,aaa,bbb", cmd);
+        else if (COMMAND_EQUAL("add_trusted_pdomains"))
+            fprintf(stdout, "%s kunteng.org.cn,apple.com,baidu.com,qq.com,aa,bb", cmd);
+        else if (COMMAND_EQUAL("add_trusted_mac"))
+            fprintf(stdout, "%s aa:bb:cc:11:22:33,11:22:33:aa:bb:cc:dd,22.22.22:aa:aa:aa", cmd);
+        else if (COMMAND_EQUAL("add_trusted_iplist"))
+            fprintf(stdout, "%s 192.168.1.2,192.168.1.3,192.168.1.4", cmd);
+        else
+            fprintf(stdout, "%s \n", cmd);
+    }
+#undef COMMAND_EQUAL
 }
 
 static void
@@ -103,16 +130,24 @@ wdctl_cmd_process(int argc, char **argv, int optind)
     }
 
     for (int i = 0; i < ARRAYLEN(wdctl_clt_cmd); i++) {
+        if (!strcmp(wdctl_clt_cmd[i].command, "help")) {
+            list_all_method();
+            return;
+        }
+
         if (!strcmp(wdctl_clt_cmd[i].command, *(argv+optind))) {
             if ((argc - (optind + 1)) > 0) {
-                wdctl_command_action(wdctl_clt_cmd[i].command, *(argv + optind + 1));
+                if (wdctl_clt_cmd[i].cmd_args)
+                    wdctl_command_action(wdctl_clt_cmd[i].command, *(argv + optind + 1));
+                else
+                    goto ERR;
             } else 
                 wdctl_command_action(wdctl_clt_cmd[i].command, NULL);
             return;
         }
     }
 ERR:
-    fprintf(stderr, "wdctl: Error: Invalid command \"%s\"\n", *(argv + optind));
+    fprintf(stderr, "wdctlx: Error: Invalid command \"%s\"\n", *(argv + optind));
     usage();
     exit(EXIT_FAILURE);
 }
@@ -214,7 +249,7 @@ wdctl_command_action(const char *cmd, const char *param)
 		asprintf(&request, "%s", cmd);
 
     send_request(sock, request);
-    free(request));
+    free(request);
     read_response(sock);
 }
 
