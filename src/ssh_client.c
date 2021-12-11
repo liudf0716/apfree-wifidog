dd940d931ef7f8875aa44ed7f20f82a70c04a363
Dengfeng Liu <liudf0716@gmail.com>
2021-12-11 21:15:35 +0800

    Update ssh_client.c

--- src/ssh_client.c
+++ src/ssh_client.c
@@ -27,11 +27,46 @@
 
 #include "ssh_client.h"
 
+#define	DEFAULT_SSH_PORT	22
+
+char *s_password;
+
+static void S_KbdCallback(const char *name, int name_len,
+                         const char *instruction, int instruction_len,
+                         int num_prompts,
+                         const LIBSSH2_USERAUTH_KBDINT_PROMPT *prompts,
+                         LIBSSH2_USERAUTH_KBDINT_RESPONSE *responses,
+                         void **abstract)
+{
+    (void)name;
+    (void)name_len;
+    (void)instruction;
+    (void)instruction_len;
+    if (num_prompts == 1)
+    {
+        responses[0].text   = s_password;
+        responses[0].length = strlen(s_password);
+    }
+    (void)prompts;
+    (void)abstract;
+}
+
 struct libssh_client *new_libssh_client(char *srv_ip, short srv_port, char ch_end, char *username, char *password)
 {
+	if (!srv_ip || !usename || !password)
+		return NULL;
+	
   struct libssh_client *ssh_client = malloc(sizeof(struct libssh_client));
   memset(ssh_client, 0, sizeof(struct libssh_client));
-  
+  strncpy(ssh_client->srv_ip, srv_ip, IPV4_LENGTH-1);
+	ssh_client->srv_port 	= srv_port == 0?DEFAULT_SSH_PORT:srv_port;
+	ssh_client->ch_end 		= ch_end;
+	ssh_client->username	= strdup(username);
+	ssh_client->password	= strdup(password);
+	s_password = ssh_client->password;
+	libssh2_init(0);
+	
+	return ssh_client;
 }
 
 void free_libssh_client(struct libssh_client *ssh_client)
@@ -49,4 +84,102 @@ void free_libssh_client(struct libssh_client *ssh_client)
   }
   libssh2_exit();
   free(ssh_client);
+}
+
+int ssh_client_connect(struct libssh_client *ssh_client)
+{
+	ssh_client->m_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(ssh_client->srv_port);
+	sin.sin_addr.s_addr = inet_addr(ssh_client->srv_ip);
+	if ( connect( ssh_client->m_sock, (sockaddr*)(&sin), sizeof(sockaddr_in) ) != 0 )
+	{
+			free_libssh_client(ssh_client);
+			return 0;
+	}
+
+	m_session = libssh2_session_init();
+	if ( libssh2_session_handshake(m_session, m_sock) )
+	{
+			free_libssh_client(ssh_client);
+			return 0;
+	}
+
+	int auth_pw = 0;
+	char *fingerprint = libssh2_hostkey_hash(ssh_client->m_session, LIBSSH2_HOSTKEY_HASH_SHA1);
+	char *userauthlist = libssh2_userauth_list( m_session, userName.c_str(), (int)userName.size() );
+	if ( strstr( userauthlist, "password") != NULL )
+	{
+			auth_pw |= 1;
+	}
+	if ( strstr( userauthlist, "keyboard-interactive") != NULL )
+	{
+			auth_pw |= 2;
+	}
+	if ( strstr(userauthlist, "publickey") != NULL)
+	{
+			auth_pw |= 4;
+	}
+
+	if (auth_pw & 1)
+	{
+		 /* We could authenticate via password */
+			if ( libssh2_userauth_password(ssh_client->m_session, ssh_client->username, ssh_client->password ) )
+			{
+					free_libssh_client(ssh_client);
+					return 0;
+			}
+	}
+	else if (auth_pw & 2)
+	{
+		 /* Or via keyboard-interactive */
+			s_password = userPwd;
+			if (libssh2_userauth_keyboard_interactive(ssh_client->m_session, ssh_client->username, &S_KbdCallback) )
+			{
+					free_libssh_client(ssh_client);
+					return 0;
+			}
+	}
+	else
+	{
+			free_libssh_client(ssh_client);
+			return 0;
+	}
+
+	return 1;
+}
+
+void ssh_client_create_channel(struct libssh_client *ssh_client, char *pty_type)
+{
+	// request a shell
+	ssh_client->m_channel = libssh2_channel_open_session(ssh_client->m_session);
+	if (!ssh_client->m_channel) {
+		return;
+	}
+	
+	/* Request a terminal with pty_type terminal emulation
+	 * See/etc/termcap for more options
+	 */
+	if ( libssh2_channel_request_pty(ssh_client->m_channel, pty_type==NULL?"vanilla":pty_type ) )
+	{
+			libssh2_channel_free(ssh_client->m_channel);
+			return;
+	}
+
+   /* Open a SHELL on that pty */
+	if ( libssh2_channel_shell(ssh_client->m_channel) )
+	{
+			libssh2_channel_free(ssh_client->m_channel);
+			return;
+	}
+}
+
+char* ssh_client_channel_read(struct libssh_client *ssh_client)
+{
+}
+
+int ssh_client_channel_write(struct libssh_client *ssh_client, char *data, int len)
+{
 }
