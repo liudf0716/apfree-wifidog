47b1a638da406c578b0e8eb79cd7205356f91930
Dengfeng Liu <liudf0716@gmail.com>
2022-01-14 10:08:19 +0800

    Update common.h

--- src/common.h
+++ src/common.h
@@ -115,7 +115,7 @@ struct roam_req_info {
     char mac[HTTP_MAC_LEN];
 };
 
-typedef roam_request_info auth_request_info;
+typedef roam_req_info auth_req_info;
 
 #ifndef is_error
 #define is_error(name)  (name == NULL)
