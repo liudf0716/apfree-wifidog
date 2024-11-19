02d9536a91b119fc52e2a16c36353ba789bdf45c
Dengfeng Liu <liudf0716@gmail.com>
2024-11-19 21:27:14 +0800

    refactor: enhance documentation in safe.c and safe.h for memory management functions
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- src/safe.c
+++ src/safe.c
@@ -10,25 +10,51 @@
 #include "debug.h"
 #include "firewall.h"
 
-/** @brief Clean up all the registered fds. */
-static void cleanup_fds(void);
+/**
+ * @file safe.c
+ * @brief Safe memory and process management functions
+ *
+ * This module provides safe versions of common memory allocation and process
+ * management functions. These functions perform error checking and handle
+ * failures appropriately.
+ */
 
-/** List of fd's to close on fork. */
+/** @brief List of file descriptors to be closed on fork */
 typedef struct _fd_list {
-    int fd;                 /**< @brief file descriptor */
-    struct _fd_list *next;  /**< @brief linked list pointer */
+    int fd;                 /**< File descriptor to be closed */
+    struct _fd_list *next;  /**< Pointer to next entry in list */
 } fd_list_t;
 
+/** @brief Head of the file descriptor cleanup list */
 static fd_list_t *fd_list = NULL;
 
-/** Clean up all the registered fds. Frees the list as it goes.
- * XXX This should only be run by CHILD processes.
+/** @brief Function pointer for custom cleanup handler */
+static void (*cleanup_func)(void) = NULL;
+
+/**
+ * @brief Set a custom cleanup function to be called on exit
+ * @param func Pointer to cleanup function
+ * 
+ * Allows setting a custom cleanup function that will be called
+ * when the program exits. This can be used to perform additional
+ * cleanup tasks beyond the standard file descriptor cleanup.
+ */
+void
+safe_set_cleanup_func(void (*func)(void))
+{
+    cleanup_func = func;
+}
+
+/**
+ * @brief Cleanup registered file descriptors
+ * 
+ * Closes all registered file descriptors and frees the list.
+ * This should only be called by child processes after fork.
  */
 static void
 cleanup_fds(void)
 {
     fd_list_t *entry;
-
     while (NULL != (entry = fd_list)) {
         close(entry->fd);
         fd_list = entry->next;
@@ -36,140 +62,139 @@ cleanup_fds(void)
     }
 }
 
-/** Register an fd for auto-cleanup on fork()
- * @param fd A file descriptor.
+/**
+ * @brief Register a file descriptor for cleanup on fork
+ * @param fd File descriptor to register
  */
 void
 register_fd_cleanup_on_fork(const int fd)
 {
     fd_list_t *entry = safe_malloc(sizeof(fd_list_t));
-
     entry->fd = fd;
     entry->next = fd_list;
     fd_list = entry;
 }
 
-/** Allocate zero-filled ram or die.
+/**
+ * @brief Allocate and zero-initialize memory
  * @param size Number of bytes to allocate
- * @return void * pointer to the zero'd bytes.
+ * @return Pointer to allocated memory
+ * @note Exits program on allocation failure
  */
 void *
 safe_malloc(size_t size)
 {
-    void *retval = NULL;
-    retval = malloc(size);
+    void *retval = malloc(size);
     if (!retval) {
-		fw_destroy();
-        debug(LOG_CRIT, "Failed to malloc %d bytes of memory: %s.  Bailing out", size, strerror(errno));
+        if (cleanup_func) {
+            cleanup_func();
+        }
+        debug(LOG_CRIT, "Failed to malloc %zu bytes: %s", size, strerror(errno));
         exit(EXIT_FAILURE);
     }
     memset(retval, 0, size);
-    return (retval);
+    return retval;
 }
 
-/** Re-allocates memory to a new larger allocation.
- * DOES NOT ZERO the added RAM
- * Original pointer is INVALID after call
- * Dies on allocation failures.
- * @param ptr A pointer to a current allocation from safe_malloc()
- * @param newsize What size it should now be in bytes
- * @return pointer to newly allocation ram
+/**
+ * @brief Safely reallocate memory
+ * @param ptr Pointer to existing allocation
+ * @param newsize New size in bytes
+ * @return Pointer to reallocated memory
+ * @note Original pointer becomes invalid after call
  */
 void *
 safe_realloc(void *ptr, size_t newsize)
 {
-    void *retval = NULL;
-    retval = realloc(ptr, newsize);
-    if (NULL == retval) {
-		fw_destroy();
-        debug(LOG_CRIT, "Failed to realloc buffer to %d bytes of memory: %s. Bailing out", newsize, strerror(errno));
+    void *retval = realloc(ptr, newsize);
+    if (!retval) {
+        if (cleanup_func) {
+            cleanup_func();
+        }
+        debug(LOG_CRIT, "Failed to realloc to %zu bytes: %s", newsize, strerror(errno));
         exit(EXIT_FAILURE);
     }
     return retval;
 }
 
-/** Duplicates a string or die if memory cannot be allocated
+/**
+ * @brief Safely duplicate a string
  * @param s String to duplicate
- * @return A string in a newly allocated chunk of heap.
+ * @return Pointer to new string
+ * @note Exits program on NULL input or allocation failure
  */
 char *
 safe_strdup(const char *s)
 {
-    char *retval = NULL;
     if (!s) {
-		fw_destroy();
-        debug(LOG_CRIT, "safe_strdup called with NULL which would have crashed strdup. Bailing out");
+        if (cleanup_func) {
+            cleanup_func();
+        }
+        debug(LOG_CRIT, "safe_strdup called with NULL input");
         exit(EXIT_FAILURE);
     }
-    retval = strdup(s);
+    char *retval = strdup(s);
     if (!retval) {
-		fw_destroy();
-        debug(LOG_CRIT, "Failed to duplicate a string: %s.  Bailing out", strerror(errno));
+        if (cleanup_func) {
+            cleanup_func();
+        }
+        debug(LOG_CRIT, "Failed to strdup: %s", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    return (retval);
+    return retval;
 }
 
-/** Sprintf into a newly allocated buffer
- * Memory MUST be freed. Dies if memory cannot be allocated.
- * @param strp Pointer to a pointer that will be set to the newly allocated string
- * @param fmt Format string like sprintf
- * @param ... Variable number of arguments for format string
- * @return int Size of allocated string.
+/**
+ * @brief Format string into newly allocated buffer
+ * @param strp Pointer to store result
+ * @param fmt Printf-style format string
+ * @return Number of bytes written
  */
 int
 safe_asprintf(char **strp, const char *fmt, ...)
 {
     va_list ap;
-    int retval;
-
     va_start(ap, fmt);
-    retval = safe_vasprintf(strp, fmt, ap);
+    int retval = safe_vasprintf(strp, fmt, ap);
     va_end(ap);
-
-    return (retval);
+    return retval;
 }
 
-/** Sprintf into a newly allocated buffer
- * Memory MUST be freed. Dies if memory cannot be allocted.
- * @param strp Pointer to a pointer that will be set to the newly allocated string
- * @param fmt Format string like sprintf
- * @param ap pre-digested va_list of arguments.
- * @return int Size of allocated string.
+/**
+ * @brief Format string with va_list into newly allocated buffer
+ * @param strp Pointer to store result
+ * @param fmt Printf-style format string
+ * @param ap Variable argument list
+ * @return Number of bytes written
  */
 int
 safe_vasprintf(char **strp, const char *fmt, va_list ap)
 {
-    int retval;
-
-    retval = vasprintf(strp, fmt, ap);
-
+    int retval = vasprintf(strp, fmt, ap);
     if (retval == -1) {
-		fw_destroy();
-        debug(LOG_CRIT, "Failed to vasprintf: %s.  Bailing out", strerror(errno));
+        if (cleanup_func) {
+            cleanup_func();
+        }
+        debug(LOG_CRIT, "Failed to vasprintf: %s", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    return (retval);
+    return retval;
 }
 
-/** Fork and then close any registered fille descriptors.
- * If fork() fails, we die.
- * @return pid_t 0 for child, pid of child for parent
+/**
+ * @brief Safely fork process and cleanup file descriptors in child
+ * @return 0 in child, child's PID in parent
+ * @note Exits program on fork failure
  */
 pid_t
 safe_fork(void)
 {
-    pid_t result;
-    result = fork();
-
+    pid_t result = fork();
     if (result == -1) {
-        debug(LOG_CRIT, "Failed to fork: %s.  Bailing out", strerror(errno));
+        debug(LOG_CRIT, "Failed to fork: %s", strerror(errno));
         exit(EXIT_FAILURE);
-    }
-	else if (result == 0) {
-        /* I'm the child - do some cleanup */
+    } else if (result == 0) {
         cleanup_fds();
     }
-
     return result;
 }
--- src/safe.h
+++ src/safe.h
@@ -7,29 +7,71 @@
 #ifndef _SAFE_H_
 #define _SAFE_H_
 
-#include <stdarg.h>             /* For va_list */
-#include <sys/types.h>          /* For fork */
-#include <unistd.h>             /* For fork */
+#include <sys/types.h>
+#include <stdarg.h>
+#include <stddef.h>
 
-/** Register an fd for auto-cleanup on fork() */
-void register_fd_cleanup_on_fork(const int);
+/**
+ * @brief Sets a cleanup function to be called during program termination
+ * 
+ * This function allows registering a cleanup callback function that will be
+ * executed when the program exits. The cleanup function can be used to perform
+ * any necessary cleanup operations like freeing resources.
+ *
+ * @param func Function pointer to the cleanup function that takes no arguments
+ *             and returns void
+ */
+void safe_set_cleanup_func(void (*func)(void));
 
-/** @brief Safe version of malloc */
-void *safe_malloc(size_t);
+/**
+ * @brief Register a file descriptor for automatic cleanup when forking
+ * @param fd File descriptor to register
+ */
+void register_fd_cleanup_on_fork(const int fd);
 
-/** @brief Safe version of realloc */
-void *safe_realloc(void *, size_t);
+/**
+ * @brief Allocate memory with error checking
+ * @param size Number of bytes to allocate
+ * @return Pointer to allocated memory
+ */
+void *safe_malloc(size_t size);
 
-/* @brief Safe version of strdup */
-char *safe_strdup(const char *);
+/**
+ * @brief Reallocate memory with error checking
+ * @param ptr Pointer to memory block to reallocate
+ * @param size New size in bytes
+ * @return Pointer to reallocated memory
+ */
+void *safe_realloc(void *ptr, size_t size);
 
-/* @brief Safe version of asprintf */
-int safe_asprintf(char **, const char *, ...);
+/**
+ * @brief Duplicate a string with error checking
+ * @param str String to duplicate
+ * @return Pointer to new string
+ */
+char *safe_strdup(const char *str);
 
-/* @brief Safe version of vasprintf */
-int safe_vasprintf(char **, const char *, va_list);
+/**
+ * @brief Format and write to allocated string with error checking
+ * @param strp Pointer to string pointer
+ * @param fmt Format string
+ * @return Number of bytes written
+ */
+int safe_asprintf(char **strp, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
 
-/* @brief Safe version of fork */
+/**
+ * @brief Format and write to allocated string with va_list
+ * @param strp Pointer to string pointer
+ * @param fmt Format string
+ * @param ap Variable argument list
+ * @return Number of bytes written
+ */
+int safe_vasprintf(char **strp, const char *fmt, va_list ap);
+
+/**
+ * @brief Fork process with error checking
+ * @return PID of child process or 0 in child
+ */
 pid_t safe_fork(void);
 
 #endif                          /* _SAFE_H_ */
