af622d543346aa9e80bf92d302e5bac07307d96e
Dengfeng Liu <liudf0716@gmail.com>
2024-12-02 11:46:59 +0800

    fix: correct macro definitions for WDCTL_TIMEOUT and WDCTL_MSG_LENG
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- src/wdctl.c
+++ src/wdctl.c
@@ -31,8 +31,8 @@
 #include "wdctl.h"
 #include "util.h"
 
-#define WDCTL_TIMEOUT   1000*2
-#define WDCTL_MSG_LENG  1024*8
+#define WDCTL_TIMEOUT   (1000*2)
+#define WDCTL_MSG_LENG  (1024*8)
 
 static char *sk_name = NULL;
 char *progname = NULL;
