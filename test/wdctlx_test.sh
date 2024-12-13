4167dc63f93524b58dd6e03077d2a3d6e8b9695f
Dengfeng Liu <liudf0716@gmail.com>
2024-12-13 10:40:00 +0800

    refactor: enhance wdctlx test script with structured command tests
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- test/wdctlx_test.sh
+++ test/wdctlx_test.sh
@@ -1,42 +1,146 @@
 #!/bin/bash
 
-echo "#################"
-echo " add trusted iplist "
-./wdctlx add_trusted_iplist 192.168.1.2,192.168.1.3,192.168.1.4,192.168.1.5
-./wdctlx show_trusted_domains
-nft list set inet fw4 set_wifidogx_trust_domains
-echo "add trusted iplist 192.1168.1.6"
-./wdctlx add_trusted_iplist 192.168.1.6
-./wdctlx show_trusted_domains
-nft list set inet fw4 set_wifidogx_trust_domains
-echo "del trusted iplist 192.1168.1.6"
-./wdctlx del_trusted_iplist 192.168.1.6
-./wdctlx show_trusted_domains
-nft list set inet fw4 set_wifidogx_trust_domains
-echo "del trusted iplist 192.1168.1.3,192.1168.1.5"
-./wdctlx del_trusted_iplist 192.168.1.3,192.168.1.5
-./wdctlx show_trusted_domains
-nft list set inet fw4 set_wifidogx_trust_domains
-echo "clear trusted iplist"
-./wdctlx clear_trusted_iplist
-./wdctlx show_trusted_domains
-nft list set inet fw4 set_wifidogx_trust_domains
+# Path to the wdctlx executable
+WDCtlx="../build/src/wdctlx"
 
-echo "#################"
-echo " add trusted mac "
-./wdctlx add_trusted_mac aa:bb:cc:11:22:33,11:22:33:aa:bb:cc,22.22.22:aa:aa:aa,33:33:33:33:33:ab,44:44:44:44:44:44,55:55:55:55:55:55
-./wdctlx show_trusted_mac
-nft list set inet fw4 set_wifidogx_trust_clients
-echo "del trusted mac 11:22:33:aa:bb:cc"
-./wdctlx del_trusted_mac 11:22:33:aa:bb:cc
-./wdctlx show_trusted_mac
-nft list set inet fw4 set_wifidogx_trust_clients
-echo "del trusted mac 55:55:55:55:55:55,33:33:33:33:33:ab"
-./wdctlx del_trusted_mac 55:55:55:55:55:55,33:33:33:33:33:ab
-./wdctlx show_trusted_mac
-nft list set inet fw4 set_wifidogx_trust_clients
-echo "clear trusted mac"
-./wdctlx clear_trusted_mac
-./wdctlx show_trusted_mac
-nft list set inet fw4 set_wifidogx_trust_clients
+# Check if wdctlx exists and is executable
+if [[ ! -x "$WDCtlx" ]]; then
+    echo "Error: wdctlx not found or not executable at $WDCtlx"
+    exit 1
+fi
 
+# Function to test wdctlx help command
+test_help() {
+    echo "Testing: wdctlx help"
+    "$WDCtlx" help
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx show commands
+test_show() {
+    echo "Testing: wdctlx show domain"
+    "$WDCtlx" show domain
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx show wildcard_domain"
+    "$WDCtlx" show wildcard_domain
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx show mac"
+    "$WDCtlx" show mac
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx add commands
+test_add() {
+    echo "Testing: wdctlx add domain example.com,example.org"
+    "$WDCtlx" add domain "example.com,example.org"
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx add wildcard_domain .example.com,.example.org"
+    "$WDCtlx" add wildcard_domain ".example.com,.example.org"
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx add mac 00:11:22:33:44:55,66:77:88:99:AA:BB"
+    "$WDCtlx" add mac "00:11:22:33:44:55,66:77:88:99:AA:BB"
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx del commands
+test_del() {
+    echo "Testing: wdctlx del mac 00:11:22:33:44:55"
+    "$WDCtlx" del mac "00:11:22:33:44:55"
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx del mac 66:77:88:99:AA:BB"
+    "$WDCtlx" del mac "66:77:88:99:AA:BB"
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx clear commands
+test_clear() {
+    echo "Testing: wdctlx clear domain"
+    "$WDCtlx" clear domain
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx clear wildcard_domain"
+    "$WDCtlx" clear wildcard_domain
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx clear mac"
+    "$WDCtlx" clear mac
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx stop command
+test_stop() {
+    echo "Testing: wdctlx stop"
+    "$WDCtlx" stop
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx reset command
+test_reset() {
+    echo "Testing: wdctlx reset value"
+    "$WDCtlx" reset value
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx status commands
+test_status() {
+    echo "Testing: wdctlx status client"
+    "$WDCtlx" status client
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx status auth"
+    "$WDCtlx" status auth
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx status wifidogx"
+    "$WDCtlx" status wifidogx
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx status"
+    "$WDCtlx" status
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx refresh command
+test_refresh() {
+    echo "Testing: wdctlx refresh"
+    "$WDCtlx" refresh
+    echo "--------------------------------------------------"
+}
+
+# Function to test wdctlx apfree commands
+test_apfree() {
+    echo "Testing: wdctlx apfree user_list"
+    "$WDCtlx" apfree user_list
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx apfree user_info"
+    "$WDCtlx" apfree user_info
+    echo "--------------------------------------------------"
+
+    echo "Testing: wdctlx apfree user_auth"
+    "$WDCtlx" apfree user_auth
+    echo "--------------------------------------------------"
+}
+
+# Execute all test functions
+test_help
+test_show
+test_add
+test_show
+test_del
+test_show
+test_clear
+test_show
+test_apfree
+test_reset
+test_status
+test_refresh
+test_stop
+
+
+echo "All tests completed."
