04dad5174f786146f304f9a89cf0fc5806a5396c
Dengfeng Liu <liudf0716@gmail.com>
2024-12-19 10:57:35 +0800

    refactor: update bypass environment script to manage dummy0 interface and adjust tap0 IP configuration
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- test/aw-create-bypass-env.sh
+++ test/aw-create-bypass-env.sh
@@ -11,6 +11,11 @@ fi
 
 # Create dummy interface in aw namespace
 echo "Creating dummy0 interface in 'aw' namespace"
+ip netns exec aw ip link show dummy0 > /dev/null 2>&1; then
+    echo "Interface 'dummy0' exists in 'aw' namespace"
+    echo "Deleting 'dummy0' interface in 'aw' namespace"
+    sudo ip netns exec aw ip link del dummy0
+fi
 sudo ip netns exec aw ip link add dummy0 type dummy
 sudo ip netns exec aw ip addr add 10.11.12.1/24 dev dummy0
 sudo ip netns exec aw ip link set dummy0 up
@@ -24,8 +29,8 @@ if ip link show tap0 > /dev/null 2>&1; then
     sudo ip link set tap0 netns aw
 
     # Set the IP address of tap0 within the aw namespace
-    echo "Setting IP address of 'tap0' to 192.168.1.2 in 'aw' namespace"
-    sudo ip netns exec aw ip addr add 192.168.1.2/24 dev tap0
+    echo "Setting IP address of 'tap0' to 100.100.100.2 in 'aw' namespace"
+    sudo ip netns exec aw ip addr add 100.100.100.2/30 dev tap0
 
     # Bring up the tap0 interface within the aw namespace
     sudo ip netns exec aw ip link set tap0 up
@@ -33,11 +38,11 @@ if ip link show tap0 > /dev/null 2>&1; then
     # Bring up the loopback interface within the aw namespace
     sudo ip netns exec aw ip link set lo up
 
-    sudo ip netns exec aw ip route add 0.0.0.0/0 via 192.168.1.1
+    sudo ip netns exec aw ip route add 0.0.0.0/0 via 100.100.100.1
 
     echo "Configuration of 'tap0' in 'aw' namespace completed"
 else
     echo "Interface 'tap0' does not exist"
 fi
 
-ip route add 100.100.100.0/24 via 172.19.100.80
+sudo ip route add 100.100.100.0/24 via 172.19.100.80
