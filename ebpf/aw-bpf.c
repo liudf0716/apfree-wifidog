becbf993ea0b9c7f0f0c518a52cd3a4aed237793
Dengfeng Liu <liudf0716@gmail.com>
2025-04-13 12:09:12 +0800

    fix: replace vmlinux.h with specific Linux headers in aw-bpf.c
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- ebpf/aw-bpf.c
+++ ebpf/aw-bpf.c
@@ -2,11 +2,17 @@
 // Copyright (C) 2025 Dengfeng Liu <liudf0716@gmail.com>
 
 #define KBUILD_MODNAME "aw-bpf"
-#include <vmlinux.h>
 #include <linux/bpf.h>
 #include <linux/pkt_cls.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/in.h>
+#include <linux/compiler.h>
 
 #include "aw-bpf.h"
 
