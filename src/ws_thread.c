a8e759f2e21a2c4f165c98075387cbea9a2c9b77
Dengfeng Liu <liudf0716@gmail.com>
2025-07-01 10:59:14 +0800

    fix: Rename cleanup_connection to cleanup_ws_connection for clarity
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- src/ws_thread.c
+++ src/ws_thread.c
@@ -95,8 +95,9 @@ static void handle_auth_request(json_object *j_auth);
 static void handle_kickoff_request(json_object *j_auth, struct bufferevent *bev);
 static void handle_get_firmware_info_request(json_object *j_req, struct bufferevent *bev);
 static void handle_firmware_upgrade_request(json_object *j_req, struct bufferevent *bev);
-static void cleanup_connection(struct bufferevent *bev);
+static void cleanup_ws_connection(void);
 static void reconnect_websocket(void);
+static void scheduled_reconnect_cb(evutil_socket_t fd, short events, void *arg);
 static void ws_send(struct evbuffer *buf, const char *msg, const size_t len, enum WebSocketFrameType frame_type);
 
 /**
